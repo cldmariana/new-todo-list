56914f66a02fa0024455557ecea39c803ace8093
claud <claudinomariana502@gmail.com>
2025-02-28 14:38:25 -0300

    start

---
+++ src/main/java/com/example/newtodolist/NewToDoListApplication.java
@@ -0,0 +1,13 @@
+package com.example.newtodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewToDoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewToDoListApplication.class, args);
+	}
+
+}
